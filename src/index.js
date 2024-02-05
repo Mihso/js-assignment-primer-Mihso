d6d7304257d8d6041c43577ccf3a390d3ac6b18c
Matthew Oshimo <moshimo90696@gmail.com>
2024-02-05 10:11:55 -0800

    all tests pass

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 export function calculateTotal (items, tax) {
-  // TODO
+  let number = 0;
+  for(let i = 0; i < items.length; i++){
+    if(items[i].taxable){
+      number += items[i].price * Math.abs(tax);
+    }
+    number += items[i].price;
+  }
+
+  return number;
 }
